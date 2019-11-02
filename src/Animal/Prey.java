878598b77133126ba07f2191bfe68b54741ceacf
Findlay Sloan <findlaysloan212@outlook.com>
2019-11-02 11:57:58 +0000

    Added the functions for hunger and mating in prey and added the predator class

--- src/Animal/Prey.java
+++ src/Animal/Prey.java
@@ -6,12 +6,14 @@ public class Prey extends Animal {
 	@Override
 	public double calculateHungerFactor() {
 		double hunger = Math.exp(-1 * hungerFactor * matingLevel);
+		hungerLevel++;
 		return hunger;
 	}
 
 	@Override
 	public double calculateMatingFactor() {
 		double mating =1 - Math.exp(-1 * matingFactor * matingLevel);
+		matingLevel++;
 		return mating;
 	}
 }
