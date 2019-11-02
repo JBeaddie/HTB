48a57cd6ca229a6cf7a1dd3639f517249e2af8fd
isaacwatson1207 <isaac.watson1207@gmail.com>
2019-11-02 11:36:17 +0000

    constructor for cell and cellbutton in development

--- src/impl/CellButton.java
+++ src/impl/CellButton.java
@@ -1,6 +1,29 @@
 package impl;
 
-import interfaces.ICellButton;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CellButton implements ICellButton {
+public class CellButton implements ActionListener {
+
+    private Cell cell;
+    private JButton button = new JButton();
+    private final Color DEFAULT_COLOUR = Color.GREEN;
+
+    public CellButton(int xcoord, int ycoord, int boardSize) {
+
+        cell = new Cell(xcoord, ycoord);
+        button.setBounds(100 + (xcoord * 300) / n, 10 + (ycoord * 300) / n, 250 / n, 250 / n);
+        button.setBackground(DEFAULT_COLOUR);
+        button.setForeground(DEFAULT_COLOUR); //EXPERIMENT WITH FOREGRND/BACKGRND
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
