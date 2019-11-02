eb52e2ba2ae918c0a057143f47821cf661d28585
Rob <robbiejohnwallace@yahoo.co.uk>
2019-11-02 13:02:58 +0000

    Added update

--- src/impl/Board.java
+++ src/impl/Board.java
@@ -5,13 +5,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class Board {
-
+    // Attributes
     private  int BOARD_SIZE;
     private CellButton[][] cellButtons;
 
     private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private boolean isStopped = true;
 
+    // Constructor
     public Board(int boardSize) {
         this.BOARD_SIZE = boardSize;
         cellButtons = new CellButton[BOARD_SIZE][BOARD_SIZE];
@@ -24,8 +25,18 @@ public class Board {
 
     }
 
+    // Methods
     public CellButton[][] getCellButtons() {
         return cellButtons;
+    }
+
+    public void update(){
+        // Loop through each cell in the board
+        for(int x = 0; x < BOARD_SIZE; x++){
+            for(int y = 0; y < BOARD_SIZE; y++){
+
+            }
+        }
     }
 
 
